dea7f396d5f3f02333769e58e2c6187f7080c9cd
sancessdaniels-maker <sancess.daniels@gmail.com>
2026-02-13 19:18:22 +0200

    taisu pushu

--- my_python_file_01.py
+++ my_python_file_01.py
@@ -1,3 +1,4 @@
 print("Hello from Visual Studio Code")
 print("Using ThinkPad with Linux on it")
 print("Maybe I should have used Ferdora instead of Ubuntu, but I'm afraid of immutable systems")
+print("I have a lot of work to do, but I will try to finish it as soon as possible")
